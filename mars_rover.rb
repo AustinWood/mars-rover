d9c94924a59073fe15591ea0889973f54b53667f
Austin Wood <austin@austinkwood.com>
2017-06-07 10:53:58 -0700

    Iterate over remaining instructions

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,13 +1,21 @@
 def engage_rovers(input)
+  # Parse intstructions into array and remove all whitespace
   instructions = input.chomp.split("\n")
   instructions.reject! { |el| el == "" }
   instructions.map! { |el| el.gsub(/\s+/, "") }
 
+  # Define a range for valid x and y coordinates
   x_range = (0..instructions[0][0].to_i)
   y_range = (0..instructions[0][1].to_i)
 
-  # rovers = []
-  instructions
+  rovers = []
+
+  (1...instructions.length).each do |i|
+    line = instructions[i]
+    p line
+  end
+
+  rovers
 end
 
 TEST_INPUT = <<~HEREDOC
@@ -18,6 +26,8 @@ TEST_INPUT = <<~HEREDOC
   LMLMLMLMM
 
   3 3 E
+
+  MMRMMRMRRM
 HEREDOC
 
 p engage_rovers(TEST_INPUT)
