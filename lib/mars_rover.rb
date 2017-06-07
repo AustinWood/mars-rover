dc282031a0b4e4f40c128d271f7f402db3ed6018
Austin Wood <austin@austinkwood.com>
2017-06-07 12:14:10 -0700

    Begin refactoring

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -1,16 +1,13 @@
 class MarsRover
   def self.engage_rovers(input)
-    # Parse intstructions into array and remove all whitespace
-    instructions = input.chomp.split("\n")
-    instructions.reject! { |el| el == "" }
-    instructions.map! { |el| el.gsub(/\s+/, "") }
+    instructions = self.parse_input(input)
 
     # Define a range for valid x and y coordinates
-    x_range = (0..instructions[0][0].to_i)
-    y_range = (0..instructions[0][1].to_i)
+    @x_range = (0..instructions[0][0].to_i)
+    @y_range = (0..instructions[0][1].to_i)
 
     # Declare an empty array to hold a hash representing each rover
-    rovers = []
+    @rovers = []
 
     # Iterate over each instruction
     (1...instructions.length).each do |i|
@@ -19,7 +16,7 @@ class MarsRover
       if i.odd?
         # If index is odd, then add new rover to array
         # with specified x, y coordinates and orientation
-        rovers.push({
+        @rovers.push({
           x: line[0].to_i,
           y: line[1].to_i,
           orientation: line[2]
@@ -27,7 +24,7 @@ class MarsRover
       else
         # Otherwise iterate over each character and mutate
         # the current rover's position and orientation accordingly
-        rover = rovers.last
+        rover = @rovers.last
         line.chars.each do |c|
           case c
           when "L"
@@ -48,19 +45,26 @@ class MarsRover
               rover[:x] -= 1
             end
 
-            unless x_range.include?(rover[:x]) && y_range.include?(rover[:y])
+            unless @x_range.include?(rover[:x]) && @y_range.include?(rover[:y])
               raise "The rover has fallen off the plateau!"
             end
           end
         end
-        rovers[rovers.length - 1] = rover
+        @rovers[@rovers.length - 1] = rover
       end
     end
 
     # Create output string in expected format
-    rovers_output = rovers.map do |rover|
+    rovers_output = @rovers.map do |rover|
       "#{rover[:x]} #{rover[:y]} #{rover[:orientation]}"
     end
     rovers_output.join("\n\n")
+  end
+
+  def self.parse_input(input)
+    # Parse input into array of instructions and remove all whitespace
+    instructions = input.chomp.split("\n")
+    instructions.reject! { |el| el == "" }
+    instructions.map! { |el| el.gsub(/\s+/, "") }
   end
 end
